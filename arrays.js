54d508c82050ab2151cd85daa8828d9594400326
vera916developer <estebantorovera916@gmail.com>
2024-05-25 15:10:09 -0500

    push arays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+const series = ["Lucifer", "Game of Thrones", 1, 2, true, false]
+console.log(series);
+console.log(series[3]);
+series.push("Berlin", true, 24);
+console.log(series);
+console.log(series.length);
+console.log(series.indexOf("got"))
+console.log(series.pop())
